96280bbb4a6be91e1e0961a4da6729d67dc50b27
Hong C <36454250+leehongc@users.noreply.github.com>
2022-12-29 16:41:27 -0800

    complete challenge 10

--- app.js
+++ app.js
@@ -31,7 +31,10 @@ app.get("/contact", function(req, res){
 });
 
 app.post("/compose", function(req, res){
-  console.log(req.body.postTitle);
+  const post = {
+    title:req.body.postTitle,
+    content:req.body.postBody
+  };
 });
 
 app.listen(3000, function() {
