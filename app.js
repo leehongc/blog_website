613fe892579f1b9af68e0b09f6e5ba36577e99d8
Hong C <36454250+leehongc@users.noreply.github.com>
2022-12-29 16:52:48 -0800

    complete challenge 11

--- app.js
+++ app.js
@@ -15,8 +15,11 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+let posts = [];
+
 app.get("/", function(req, res){
   res.render('home', {homeStarterText:homeStartingContent});
+  console.log(posts);
 });
 app.get("/compose", function(req, res){
   res.render('compose');
@@ -35,6 +38,8 @@ app.post("/compose", function(req, res){
     title:req.body.postTitle,
     content:req.body.postBody
   };
+  posts.push(post);
+  res.redirect('/');
 });
 
 app.listen(3000, function() {
