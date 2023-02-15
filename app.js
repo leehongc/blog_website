6ef65ebd621e40d5b88693f15a47b5a410518f72
Hong C <36454250+leehongc@users.noreply.github.com>
2023-02-14 21:38:26 -0800

    add callback function to post.save()

--- app.js
+++ app.js
@@ -55,8 +55,11 @@ app.post("/compose", function(req, res){
     content:req.body.postBody
   });
 
-  post.save();
-  res.redirect('/');
+  post.save(function(err){
+    if(!err){
+      res.redirect('/');
+    }
+  });
 });
 
 app.get("/posts/:postName", function(req, res){
